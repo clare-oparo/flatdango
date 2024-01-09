4ca15873782c5cfd16bc33933f8fc11bebe6a732
Clare <clare.oparo@student.moringaschool.com>
2024-01-09 16:15:28 +0300

    edited syntax

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function()){
+document.addEventListener('DOMContentLoaded', function() {
     fetch(' http://localhost:3000/films/1')
     .then(response => response.json)
     .then(data => showMovieDetails(data[0]));
-}
+})
 function showMovieDetails(movie){
     const availableTickets = movie.capacity-movie.ticketsSold;
     const movieDeets = document.getElementById('movieDetails');
