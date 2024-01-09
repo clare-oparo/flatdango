3eb90253d019b010636db4fd20553a05a4cd3ad2
Clare <clare.oparo@student.moringaschool.com>
2024-01-09 16:28:46 +0300

    updated function>

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => showMovieDetails(data[0]));
 })
 function showMovieDetails(movie){
-    const availableTickets = movie.capacity-movie.ticketsSold;
+    const availableTickets = movie.capacity-movie.tickets_sold;
     const movieDeets = document.getElementById('movieDetails');
+    movieDeets.innerHTML = 
+    `<h1>${movie.title}</h1>
+    <img src = ${movie.poster}>
+    <p>Runtime: ${movie.runtime} minutes</p>
+    <p>Showtime: ${movie.showtime}</p>
+    <p>Available Tickets: ${availableTickets} ONLY!</p>`
 }
